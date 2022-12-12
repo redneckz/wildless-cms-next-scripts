d43cdac9b073d2473628485ca15d62a216ed0e2d
redneckz <vasyaselsky@yandex.ru>
2022-12-12 15:36:53 +0300

    Add support of named slots

--- scripts/getBlockTypes.js
+++ scripts/getBlockTypes.js
@@ -2,18 +2,19 @@ export const extractType = ({ type }) => type;
 export const extractMobileType = ({ type, mobile: { type: mobileType } = {} }) =>
   mobileType || type;
 
-const extractAllTypes = (blocks, typeExtractor) =>
-  blocks.flatMap((b) => [typeExtractor(b), ...extractAllTypes(b.blocks || [], typeExtractor)]);
+const extractAllTypes = (typeExtractor) => (slot) =>
+  (typeExtractor(slot) ? [typeExtractor(slot)] : []).concat(
+    slot.blocks?.flatMap(extractAllTypes(typeExtractor)),
+    Object.values(slot.slots || {}).flatMap((list) =>
+      list?.flatMap(extractAllTypes(typeExtractor)),
+    ),
+  );
 
 export const getBlockTypes =
   ({ blocksRegistry, typeExtractor }) =>
   (page) =>
     unique(
-      extractAllTypes(page.blocks || [], typeExtractor)
-        .filter((type) => type && type in blocksRegistry)
-        .concat(
-          ...Object.values(page.slots || {}).map(getBlockTypes({ blocksRegistry, typeExtractor })),
-        ),
+      extractAllTypes(typeExtractor)(page || {}).filter((type) => type && type in blocksRegistry),
     );
 
 function unique(list) {
