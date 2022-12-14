e3c99ca5bbf77b9d7ff9cb354a9d59bb4842225f
redneckz <vasyaselsky@yandex.ru>
2022-12-14 14:00:41 +0300

    Remove ContentPageContext usage

--- scripts/page.tmpl.tsx
+++ scripts/page.tmpl.tsx
@@ -1,31 +1,29 @@
+import { useRouter } from '@redneckz/wildless-cms-uni-blocks';
 import { computeAPIFallback } from '@redneckz/wildless-cms-uni-blocks/lib/components/ContentPage/computeAPIFallback';
 import { ContentPage } from '@redneckz/wildless-cms-uni-blocks/lib/components/ContentPage/ContentPage';
-import type { ContentPageContext } from '@redneckz/wildless-cms-uni-blocks/lib/components/ContentPage/ContentPageContext';
 import { normalizePage } from '@redneckz/wildless-cms-uni-blocks/lib/components/ContentPage/normalizePage';
 import { renderContentPageHead } from '@redneckz/wildless-cms-uni-blocks/lib/components/ContentPage/renderContentPageHead';
 import { ContentPageRepository } from '@redneckz/wildless-cms-uni-blocks/lib/content-page-repository';
 import type { ContentPageDef } from '@redneckz/wildless-cms-uni-blocks/lib/model/ContentPageDef';
 import type { GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
+import { useRouter as nextUseRouter } from 'next/router';
 /* imports */
 
 const blocksRegistry = {
   /* blocks */
 };
 
-const contentPageContext: ContentPageContext = {
-  useRouter() {
-    const { asPath: pathname, ...rest } = useRouter();
+useRouter.setup(() => {
+  const { asPath: pathname, ...rest } = nextUseRouter();
 
-    return { ...rest, pathname, href: globalThis.location?.href };
-  },
-};
+  return { ...rest, pathname, href: globalThis.location?.href };
+});
 
 const GenericPage: NextPage<{ data: ContentPageDef }> = ({ data }) => (
   <>
     <Head>{renderContentPageHead(data)}</Head>
-    <ContentPage data={data} context={contentPageContext} blocksRegistry={blocksRegistry} />
+    <ContentPage data={data} blocksRegistry={blocksRegistry} />
   </>
 );
 
