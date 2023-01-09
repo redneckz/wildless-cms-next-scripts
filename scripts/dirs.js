2aad0e569f12a27dfc95d694a200afebfa42ec78
vladeiko <vladeiko999@gmail.com>
2023-01-09 13:14:40 +0300

    Add custom blocks support

--- scripts/dirs.js
+++ scripts/dirs.js
@@ -3,3 +3,4 @@ export const PAGES_DIR = 'pages';
 export const PUBLIC_DIR = 'public';
 export const BUILD_DIR = 'build';
 export const EXTRA_DIR = 'extra';
+export const CUSTOM_BLOCKS_DIR = 'src/custom/blocks';
