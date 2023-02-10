7bb73cb6aba9d3875b86924c8bf65bef4b7c7c6f
redneckz <vasyaselsky@yandex.ru>
2023-02-10 11:21:04 +0300

    Include unused attachments clean up to prebuild phase

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,6 +1,7 @@
 import copyfiles from 'copyfiles';
 import { promisify } from 'util';
 import { BUILD_DIR, CONTENT_DIR, NEXT_DIR, PAGES_DIR, PUBLIC_DIR } from './dirs.js';
+import removeDetached from './removeDetached.js';
 import { gitClean } from './utils/gitClean.js';
 import { rmrf } from './utils/rmrf.js';
 
@@ -8,7 +9,13 @@ const copy = promisify(copyfiles);
 
 export default async function prebuild() {
   await rmrf(BUILD_DIR, NEXT_DIR);
-  await gitClean(PUBLIC_DIR, PAGES_DIR);
+  await gitClean(CONTENT_DIR, PUBLIC_DIR, PAGES_DIR);
+
+  try {
+    await removeDetached();
+  } catch (ex) {
+    // Do nothing
+  }
 
   await copy([`${CONTENT_DIR}/wcms-resources/**/*`, PUBLIC_DIR], 1);
 }
