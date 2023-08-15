046ff0835903a856f6274bf5a11da9f98797d7e0
redneckz <vasyaselsky@yandex.ru>
2023-08-15 20:42:21 +0300

    Optimize gzip build step

--- gz.sh
+++ gz.sh
@@ -2,14 +2,19 @@
 
 COMPRESSION_RATIO=80
 
+size() {
+  wc -c <"$1"
+}
+
 find "$1" -type f -not -name '*.webp' -not -name '*.gz' |
   while read -r filename; do
-    source_size=$(wc -c <"$filename")                 # Размер в байтах несжатого файла
-    gzipped_size=$(gzip -c -k -9 "$filename" | wc -c) # Размер в байтах сжатого файла (тестовый прогон)
+    gzip -k -9 "$filename"
 
-    test_size=$((source_size * COMPRESSION_RATIO / 100))
+    source_size=$(size "$filename")
+    gzipped_size=$(size "$filename".gz)
+    test_size=$(( source_size * COMPRESSION_RATIO / 100 ))
 
-    if [ $gzipped_size -le $test_size ]; then
-      gzip -k -9 "$filename"
+    if [ $gzipped_size -ge $test_size ]; then
+      rm -f "$filename".gz
     fi
   done
