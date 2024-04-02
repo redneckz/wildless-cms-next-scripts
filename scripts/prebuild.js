1e3e4c6e0cadadd8b2f9bd4ac3f0731fb2be9176
Igor Bakhtin <mikage345@gmail.com>
2024-04-02 20:23:35 +0500

    remove script removeDetached.js

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,18 +1,10 @@
 import createInfoFile from './createInfoFile.js';
 import { BUILD_DIR, NEXT_DIR, PUBLIC_DIR } from './utils/env.js';
-import removeDetached from './removeDetached.js';
 import { gitClean } from './utils/gitClean.js';
 import { rmrf } from './utils/rmrf.js';
 
 export default async function prebuild() {
   await rmrf(BUILD_DIR, NEXT_DIR);
   await gitClean(PUBLIC_DIR);
-
-  try {
-    await removeDetached();
-  } catch (ex) {
-    // Do nothing
-  }
-
   await createInfoFile();
 }
