812efe9a7102ffef984be441b7e09bd10ebfcb99
Igor Bakhtin <mikage345@gmail.com>
2024-11-27 14:11:15 +0500

    remove NODE_TLS_REJECT_UNAUTHORIZED

--- scripts/build.js
+++ scripts/build.js
@@ -3,8 +3,6 @@ import copyExtra from './extra.js';
 import { generate } from './generate.js';
 import stats from './stats.js';
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0; //? Only for build
-
 export default async function build({ isMobile, sitemap, ssg, extra }) {
   try {
     await stats();
